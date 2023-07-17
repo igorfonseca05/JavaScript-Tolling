2767d92ca433fa00361b8a023da2400467f59d51
Igor R da Fonseca <igorfonseca27@hotmail.com>
2023-07-17 11:04:20 -0300

    Arquivos importantes
    Salvando arquivos do JavaScript Tolling

---
+++ ModeloWebPack-semCSS/public/assets/js/bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// 1
+// 2
+// 3
+
+console.log('oii');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
